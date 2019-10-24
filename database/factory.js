41fe4eb06ee08fe7ad9a27367363e3c638e4ee02
Vicente Brida <vicentebrida@outlook.com>
2019-10-23 22:31:15 -0300

    Add authentication

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,14 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', (faker, i, data = {}) => {
+  return {
+    nome: faker.name(),
+    nome_guerra: faker.name(),
+    email: faker.email(),
+    password: faker.string(),
+    ...data
+  };
+})
